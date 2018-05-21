bf7bc890e5f1a7cc72e50f6ab49767bd8fea1101
Unknown <tevit.se598@cbs.chula.ac.th>
2018-05-22 01:07:08 +0700

    Start Project S&D

---
+++ Controllers/SDController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewSD.Models;
+
+
+namespace NewSD.Controllers
+{
+    public class SDController : Controller
+    {
+        // GET: SD
+        public ActionResult Login()
+        {
+            return View();
+        }
+       
+
+        [HttpPost]
+        public ActionResult LoginbyUsernameandPassword(string UserName,string Password)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: SubscribeUser(model.Email);
+            }
+            return View("Index");
+        }
+    }
+}
